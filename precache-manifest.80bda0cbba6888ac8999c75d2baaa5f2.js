6ad7f1ff8788d20916f7be3356c00d1e97d013a2
rgabs <rahulgaba16@gmail.com>
2019-01-23 23:57:46 +0100

    Updates

---
+++ precache-manifest.80bda0cbba6888ac8999c75d2baaa5f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eb4f934c5b997f27646",
+    "url": "/static/css/main.58486f79.chunk.css"
+  },
+  {
+    "revision": "2eb4f934c5b997f27646",
+    "url": "/static/js/main.2eb4f934.chunk.js"
+  },
+  {
+    "revision": "2c2bd8a7d7c86afe2e80",
+    "url": "/static/js/1.2c2bd8a7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0cdf8827eae73036e66e7906c759bb52",
+    "url": "/index.html"
+  }
+];

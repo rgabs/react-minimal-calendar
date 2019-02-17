d6b391f91206f1a0c8d82e9e62e810f6c87229e7
rgabs <rahulgaba16@gmail.com>
2019-02-17 23:49:49 +0100

    Extending airbnb’s ESLint config

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-    "extends": "react-app",
+    "extends": [
+        "react-app",
+        "airbnb"
+    ],
     "rules" : {
-        "indent": ["error", 4]
+        "indent": ["error", 4],
+        "react/destructuring-assignment": 0,
+        "react/jsx-filename-extension": 0
         // more custom rules here
     }
 }
